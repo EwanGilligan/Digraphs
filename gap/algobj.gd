dc49c12f715004601c9e9e1d4725ba5c3d316806
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-11-29 16:59:23 +0000

    Add filters for the two newly added algorithms

--- gap/algobj.gd
+++ gap/algobj.gd
@@ -1,2 +1,5 @@
 DeclareCategory("IsDigraphAlgorithm", IsObject);
 DeclareCategory("IsDigraphColouringAlgorithm", isDigraphAlgorithm);
+# Filters for specific graph colouring algorithm implementations
+DeclareFilter("IsDigraphColouringAlgorithmByskov");
+DeclareFilter("IsDigraphColouringAlgorithmLawler");
